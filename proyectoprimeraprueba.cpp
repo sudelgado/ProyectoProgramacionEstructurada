49d0c68bc54a8c797d1c9d704a713e207c5e000f
Su <nasurydelb927@gmail.com>
2026-08-18 18:55:03 -0600

    Fix formatting and improve menu option messages
    Cambie mayusculas y puntos

--- proyectoprimeraprueba.cpp
+++ proyectoprimeraprueba.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 int main(){
 
-int i =0;
+int i=0;
 
 
 do {
 	system("cls");
 		std::cout<<"\n        				 << MINI GIT >>\n";
 		std::cout<<"					<<1. Inicializar repositorio.>>\n";
-		std::cout<<"					<<2. Ver informacion del repositorio>>\n";
+		std::cout<<"					<<2. Ver Informacion del Repositorio>>\n";
 		std::cout<<"					<<3. Simular Cambios.>>\n";
 		std::cout<<"					<<4. Crear Commit>>\n";
 		std::cout<<"					<<5. Ver Ultimo Commit>>\n";
-		std::cout<<"					<<6. Ver estado del repositorio>>\n";
-		std::cout<<"					<<7. Reiniciar repositorio>>\n";
+		std::cout<<"					<<6. Ver Estado del Repositorio>>\n";
+		std::cout<<"					<<7. Reiniciar Repositorio>>\n";
 		std::cout<<"					<<8. Salir>>\n";
 		std::cin>> i;
 		
@@ -55,12 +55,12 @@ do {
 				system("cls");
 			case 8:
 				system("cls");
-			std::cout<<"saliendo....";
+			std::cout<<"Saliendo....";
 			
 			break;
 				
 			default:
-				std::cout<<"opcion no valida ingrese otra: ";
+				std::cout<<"Opcion no valida. Ingrese otra: ";
 				break;
 		}
 
