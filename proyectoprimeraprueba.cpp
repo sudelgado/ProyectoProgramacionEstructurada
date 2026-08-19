1bd8e95a6a7ab91e104f4b36faf4dce57c28c343
Su <nasurydelb927@gmail.com>
2026-08-18 18:49:25 -0600

    Refactor main function and clean up code

--- proyectoprimeraprueba.cpp
+++ proyectoprimeraprueba.cpp
@@ -9,7 +9,7 @@ do {
 		std::cout<<"\n        				 << MINI GIT >>\n";
 		std::cout<<"					<<1. Inicializar repositorio.>>\n";
 		std::cout<<"					<<2. Ver informacion del repositorio>>\n";
-		std::cout<<	"					<<3. Simular Cambios.>>\n";
+		std::cout<<"					<<3. Simular Cambios.>>\n";
 		std::cout<<"					<<4. Crear Commit>>\n";
 		std::cout<<"					<<5. Ver Ultimo Commit>>\n";
 		std::cout<<"					<<6. Ver estado del repositorio>>\n";
