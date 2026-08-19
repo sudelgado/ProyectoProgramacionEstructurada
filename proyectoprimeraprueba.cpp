493437bb1c26c7d4d94858ef2081be0193ff4f9c
Jayjay <40924248+yader1234@users.noreply.github.com>
2026-08-18 20:37:32 -0600

    Update proyectoprimeraprueba.cpp

--- proyectoprimeraprueba.cpp
+++ proyectoprimeraprueba.cpp
@@ -7,9 +7,9 @@ int i=0;
 do {
 	system("cls");
 		std::cout<<"\n        				 << MINI GIT >>\n";
-		std::cout<<"					<<1. Inicializar repositorio.>>\n";
+		std::cout<<"					<<1. Inicializar Repositorio>>\n";
 		std::cout<<"					<<2. Ver Informacion del Repositorio>>\n";
-		std::cout<<"					<<3. Simular Cambios.>>\n";
+		std::cout<<"					<<3. Simular Cambios>>\n";
 		std::cout<<"					<<4. Crear Commit>>\n";
 		std::cout<<"					<<5. Ver Ultimo Commit>>\n";
 		std::cout<<"					<<6. Ver Estado del Repositorio>>\n";
@@ -20,37 +20,45 @@ do {
 		switch(i){
 			
 			case 1:
-				
+				system("cls");
+				std::cout<<"Opcion digitada: 1...\nEntrando a: Inicializando Repositorio\n";
 				
 				break;
 				system("cls");
 			case 2:
-			
+			    system("cls");
+				std::cout<<"Opcion digitada: 2...\nEntrando a: Informacion del Repositorio\n";
+					
 				break;
 				system("cls");
 			case 3:
 				system("cls");
-			
+			    std::cout<<"Opcion digitada: 3...\nEntrando a: Simulador de Cambios\n";
+				
 				break;
 				system("cls");
 			case 4:
 				system("cls");
-			
+			    std::cout<<"Opcion digitada: 4...\nEntrando a: Crear Commit\n";
+				
 				break;
 				system("cls");
 			case 5:
 				system("cls");
-			
+			    std::cout<<"Opcion digitada: 5...\nEntrando a: Ver el Ultimo Commit\n";
+				
 				break;
 				system("cls");
 			case 6:
 				system("cls");
-			
+			    std::cout<<"Opcion digitada: 6...\nEntrando a: Estado del Repositorio\n";
+				
 				break;
 				system("cls");
 			case 7:
 				system("cls");
-			
+			    std::cout<<"Opcion digitada: 7...\nEntrando a: Reiniciar Repositorio\n";
+				
 				break;
 				system("cls");
 			case 8:
