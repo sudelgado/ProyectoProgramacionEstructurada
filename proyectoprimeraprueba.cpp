d437a6e570fd3cb86131110b1ca2696fb091c14d
Jayjay <40924248+yader1234@users.noreply.github.com>
2026-08-18 18:42:41 -0600

    proyecto-zzz

---
+++ proyectoprimeraprueba.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+int main(){
+
+int i =0;
+
+
+do {
+	system("cls");
+		std::cout<<"\n        		 << MINI GIT >>\n";
+		std::cout<<"		<<1. Inicializar repositorio.>>\n";
+		std::cout<<"		<<2. Ver informacion del repositorio>>\n";
+		std::cout<<	"		<< 3.Simular Cambios.>>\n";
+		std::cout<<"		<<4. Crear Commit>>\n";
+		std::cout<<"		<<5. Ver Ultimo Commit>>\n";
+		std::cout<<"		<<6. Ver estado del repositorio>>\n";
+		std::cout<<"		<<7. Reiniciar repositorio>>\n";
+		std::cout<<"		<<8 .salir>>\n";
+		std::cin>> i;
+		
+		switch(i){
+			
+			case 1:
+				
+				
+				break;
+				system("cls");
+			case 2:
+			
+				break;
+				system("cls");
+			case 3:
+				system("cls");
+			
+				break;
+				system("cls");
+			case 4:
+				system("cls");
+			
+				break;
+				system("cls");
+			case 5:
+				system("cls");
+			
+				break;
+				system("cls");
+			case 6:
+				system("cls");
+			
+				break;
+				system("cls");
+			case 7:
+				system("cls");
+			
+				break;
+				system("cls");
+			case 8:
+				system("cls");
+			std::cout<<"saliendo....";
+			
+			break;
+				
+			default:
+				std::cout<<"opcion no valida ingrese otra: ";
+				break;
+				system("cls");
+		}
+
+	}
+	while (i!=8);
+		return 0;
+}
+
