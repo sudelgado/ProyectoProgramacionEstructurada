7b33302a23c2cfa9ff0c4310cff6dc1d88d48631
genesismongeduran <genesis.monge.duran@est.una.ac.cr>
2026-08-29 19:21:04 -0600

    Update proyectoprimeraprueba.cpp
    meti un cambio en la variable de estado y la cascara del switch del case 3

--- proyectoprimeraprueba.cpp
+++ proyectoprimeraprueba.cpp
@@ -3,8 +3,8 @@
 #include <iomanip>
 int main(){
 
-int i=0, cantidadCommits=0;
-std::string nombrerepositorio, autor;
+int i=0, cantidadCommits=0, opcion=0;
+std::string nombrerepositorio, autor, estado;
 bool repositorioCreado = false, cambiosPendientes= false;
 
 do {
@@ -74,7 +74,7 @@ if (std::cin.fail()) {
 
     		system("pause");
    			 break;
-   
+   		}
 			case 2:
 			    system("cls");
 				std::cout<<"Opcion digitada: 2...\nEntrando a: Informacion del Repositorio\n\n";
@@ -85,9 +85,12 @@ if (std::cin.fail()) {
 						system("pause");
 					}
 				else {
+					estado = "Inicializado";
+					estado += (cambiosPendientes) ? " | Cambios pendientes" : " | Sin cambios pendientes"; // aqui cambia el valor de estado dependiendo del valor de cambios pendientes
+					
 					std::cout << "					Repositorio: " << nombrerepositorio << "\n";
 					std::cout << "					Autor del repositorio: " << autor << "\n";
-					std::cout << "					Estado: Inicializado\n"; //revisar como seria en caso de que no este inicializado ya que no use variable
+					std::cout << "					Estado: " << estado << "\n"; //revisar como seria en caso de que no este inicializado ya que no use variable
 					std::cout << "					Cantidad de commits: " << cantidadCommits << "\n";
 					std::cout << "					Ultimo commit: #" << cantidadCommits << "\n";
 					std::cout << "					Cambios pendientes: " << (cambiosPendientes ? "Si": "No") << "\n";
@@ -98,9 +101,61 @@ if (std::cin.fail()) {
 				break;
 				system("cls");
 			case 3:
+				
 				system("cls");
 			    std::cout<<"Opcion digitada: 3...\nEntrando a: Simulador de Cambios\n";
 				
+				std::cout << "\n        				 << SIMULADOR DE CAMBIOS >>\n";
+				
+				std::cout<<" 					<<1. Agregar Archivo>>\n";
+				std::cout<<"					<<2. Modificar archivo>>\n";
+				std::cout<<"					<<3. Eliminar archivo>>\n";
+				std::cout<<"					<<4. Cancelar>>\n";
+				
+				std::cin>> opcion; //revisen si hay que cambiar el nombre de la variable para mas adelante
+		
+				if (std::cin.fail()) { //meti esta estructura por si daba el mismo bug que antes
+   					std::cin.clear();
+    				std::cin.ignore(1000, '\n');
+
+    				std::cout << "Opcion no valida. Debe ingresar un numero.\n";
+    				system("pause");
+  					  continue;
+			}
+				switch (opcion) {
+					
+					case 1:
+						system("cls");
+						std::cout <<"Opcion digitada: 1...\n Entrando a: Agregar archivo\n\n";
+						system("pause");
+        				break;
+
+    				case 2:
+       					system("cls");
+       					std::cout << "Opcion digitada: 2...\nEntrando a: Modificar Archivo\n\n";
+						system("pause");
+       					break;
+
+   					case 3:
+        				system("cls");
+       					std::cout << "Opcion digitada: 3...\nEntrando a: Eliminar Archivo\n\n";
+
+       					system("pause");
+       					break;
+
+   					case 4:
+       					system("cls");
+      					std::cout << "Opcion digitada: 4...\nCancelando...\n";
+       					system("pause");
+       					break;
+
+   					default:
+        				std::cout << "Opcion no valida. Digite una opcion del SUBMENU.\n";
+       					system("pause");
+        				break;
+				}
+
+				system("pause");
 				break;
 				system("cls");
 			case 4:
@@ -139,11 +194,12 @@ if (std::cin.fail()) {
 				break;
 		}
 
-	}
-}	while (i!=8);
+	} while (i!=8);
 	
 	
 
 		return 0;
+		
+		//agregue la variable estado para cuando mas adelante se necesite revisar si hay cambios pendientes en el repo, entonces es bueno trabajarla por aparte para no usarla como string si no se cambie sola por el estado de la variable
 
 }
